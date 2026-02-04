a3082b89a0bde3805dfc28c7bc1d72e9ee13dc4b
Mullah98 <ibz_374@hotmail.com>
2026-02-04 00:45:38 +0000

    update server.js to include health-route

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
 const app = express()
+
 app.use(express.static('dist'))
 app.get('/health', (req, res) => res.json({ status: 'ok' }))
+
 app.listen(3000, () => console.log('Listening on port 3000'))
