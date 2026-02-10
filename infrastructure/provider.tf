247a6cf4b4b79a7e61acc4b6fec97304c76027d3
Mullah98 <ibz_374@hotmail.com>
2026-02-10 21:53:58 +0000

    add cloudflare provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
     aws = {
         source = "hashicorp/aws"
         version = "~> 6.0"
     }
   }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
 
 provider "aws" {
