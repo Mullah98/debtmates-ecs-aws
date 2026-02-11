548c79c49b886547af4d6a5b7de729407edf0df1
Mullah98 <ibz_374@hotmail.com>
2026-02-11 16:37:02 +0000

    complete alb module configurations

---
+++ infrastructure/modules/alb/main.tf
@@ -0,0 +1,93 @@
+## Create load balancer security group with 2 inbound listeners (port 80 & port 443). Allow all outbound traffic
+
+resource "aws_security_group" "alb_sg" {
+  name = "alb-security-group"  
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+## Create application load balancer
+
+resource "aws_lb" "alb" {
+  name = "application-load-balancer"  
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets = var.public_subnet_ids
+
+  tags = {
+    Name = "alb"
+  }
+}
+
+## Create target groups
+
+resource "aws_lb_target_group" "alb_tg" {
+  name = "alb-target-group"
+  port = 3000
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id = var.vpc_id
+
+  health_check {
+    path = "/health"
+    matcher = "200"
+  }
+}
+
+## Create http listener
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+## Create https listener
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port = "443"
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = var.certificate_arn
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
