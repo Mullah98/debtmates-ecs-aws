f4cecf21528591a11efc4d13a4d030ac9b4ebc83
Mullah98 <ibz_374@hotmail.com>
2026-02-11 00:46:38 +0000

    add acm module in root main.tf. create acm variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,3 +5,11 @@ module "vpc" {
 module "ecr" {
   source = "./modules/ecr"
 }
+
+module "acm" {
+  source = "./modules/acm"
+
+  cloudflare_zone_id = var.cloudflare_zone_id
+  domain_name = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,17 @@ variable "cloudflare_api_token" {
   type = string
   sensitive = true
 }
+
+variable "domain_name" {
+  type = string
+  default = "ibrahimdevops.co.uk"
+}
+
+variable "subject_alternative_names" {
+  type = list(string)
+  default = ["*.ibrahimdevops.co.uk"]
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
