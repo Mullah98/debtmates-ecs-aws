014ae85fa46992105691624eb522ecf0356c97d0
Mullah98 <ibz_374@hotmail.com>
2026-02-12 00:15:49 +0000

    add alb module to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,3 +13,11 @@ module "acm" {
   domain_name = var.domain_name
   subject_alternative_names = var.subject_alternative_names
 }
+
+module "alb" {
+  source = "./modules/alb"
+
+  certificate_arn = module.acm.arn
+  vpc_id = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+}
