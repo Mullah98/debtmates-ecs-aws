934669d639062b12506ebdf4686a46426e2e6581
Mullah98 <ibz_374@hotmail.com>
2026-02-14 15:51:30 +0000

    fix var syntax for public subnets

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -111,7 +111,7 @@ resource "aws_route_table" "private_route_table_2b" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "var.ipv4_default_route"
+    cidr_block = var.ipv4_default_route
     nat_gateway_id = aws_nat_gateway.nat_2.id
   }
 
