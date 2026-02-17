bf451426b13d88c816cf9491be11ba00ae2e7e97
Mullah98 <ibz_374@hotmail.com>
2026-02-17 22:09:06 +0000

    add custom policies to all modules

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -2,9 +2,7 @@
 
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
   client_id_list = ["sts.amazonaws.com"]
-
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
@@ -38,11 +36,13 @@ resource "aws_iam_role_policy" "github_actions_policy" {
     Version = "2012-10-17",
     Statement = [
         {
+            Sid = "ECRAuthToken",
             Effect = "Allow",
             Action = ["ecr:GetAuthorizationToken"],
             Resource = "*"
         },
         {
+            Sid = "ECRAccess",
             Effect = "Allow",
             Action = [
             "ecr:BatchCheckLayerAvailability",
@@ -54,6 +54,83 @@ resource "aws_iam_role_policy" "github_actions_policy" {
             "ecr:BatchGetImage"
         ],
             Resource = var.ecr_repository_arn
+        },
+        {
+            Sid = "S3ListBucket",
+            Effect = "Allow",
+            Action = ["s3:ListBucket"],
+            Resource = var.s3_bucket_arn
+        },
+        {
+            Sid = "S3Access",
+            Effect = "Allow",
+            Action = [
+            "s3:GetObject", 
+            "s3:PutObject", 
+            "s3:DeleteObject"
+            ],
+            Resource = "${var.s3_bucket_arn}/*"
+        },
+        {
+            Sid = "DynamoDBAccess",
+            Effect = "Allow",
+            Action = [
+                "dynamodb:GetItem", 
+                "dynamodb:PutItem", 
+                "dynamodb:DeleteItem", 
+                "dynamodb:UpdateItem",
+                "dynamodb:DescribeTable"
+            ],
+            Resource = var.dynamodb_table_arn
+        },
+        {
+            Sid = "ECSAccess",
+            Effect = "Allow",
+            Action = ["ecs:*", "logs:*"],
+            Resource = "*"
+        },
+        {
+            Sid = "EC2FullAccess",
+            Effect = "Allow",
+            Action = ["ec2:*"],
+            Resource = "*"
+        },
+        {
+            Sid = "IAMAccess",
+            Effect = "Allow",
+            Action = [
+                "iam:GetRole",
+                "iam:CreateRole",
+                "iam:DeleteRole",
+                "iam:UpdateRole",
+                "iam:PutRolePolicy",
+                "iam:DeleteRolePolicy",
+                "iam:GetRolePolicy",
+                "iam:PassRole",
+                "iam:ListAttachedRolePolicies",
+                "iam:AttachRolePolicy",
+                "iam:DetachRolePolicy"
+            ],
+            Resource = "*"
+        },
+        {
+            Sid = "ACMAccess",
+            Effect = "Allow",
+            Action = [
+                "acm:RequestCertificate",
+                "acm:DescribeCertificate",
+                "acm:DeleteCertificate",
+                "acm:ListCertificates",
+                "acm:AddTagsToCertificate",
+                "acm:RemoveTagsFromCertificate"
+            ],
+            Resource = "*"
+        },
+        {
+            Sid = "ELBAccess",
+            Effect = "Allow",
+            Action = ["elasticloadbalancing:*"],
+            Resource = "*"
         }
     ]
   })
--- infrastructure/modules/iam/variables.tf
+++ infrastructure/modules/iam/variables.tf
@@ -12,3 +12,13 @@ variable "ecr_repository_arn" {
   description = "ARN of the ECR repository"  
   type = string
 }
+
+variable "s3_bucket_arn" {
+  description = "ARN of the S3 Bucket"
+  type = string
+}
+
+variable "dynamodb_table_arn" {
+  description = "ARN of the DynamoDB table"
+  type = string
+}
