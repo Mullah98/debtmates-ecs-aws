bc518790512be13fe177fddd02a6791e3e16a583
Mullah98 <ibz_374@hotmail.com>
2026-02-21 02:15:22 +0000

    change value key to content

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -18,8 +18,8 @@ resource "cloudflare_dns_record" "acm_validation" {
     for dvo in aws_acm_certificate.cert.domain_validation_options :
     dvo.domain_name => {
       name  = dvo.resource_record_name
+      content = dvo.resource_record_value
       type  = dvo.resource_record_type
-      value = dvo.resource_record_value
     }
   }
 
