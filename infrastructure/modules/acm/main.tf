f42f60eb73a281a8448892a3f724446e40fb50c4
Mullah98 <ibz_374@hotmail.com>
2026-02-21 02:16:20 +0000

    format acm module

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -17,9 +17,9 @@ resource "cloudflare_dns_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options :
     dvo.domain_name => {
-      name  = dvo.resource_record_name
+      name    = dvo.resource_record_name
       content = dvo.resource_record_value
-      type  = dvo.resource_record_type
+      type    = dvo.resource_record_type
     }
   }
 
