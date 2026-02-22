80c6d297da76f2920441583604f33c2c1f79b6f3
Mullah98 <ibz_374@hotmail.com>
2026-02-22 14:33:10 +0000

    remove indexing from dns record loop

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -15,7 +15,7 @@ resource "aws_acm_certificate" "cert" {
 resource "cloudflare_dns_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options :
-    dvo.resource_record_name => {
+    dvo.domain_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
@@ -23,9 +23,9 @@ resource "cloudflare_dns_record" "acm_validation" {
   }
 
   zone_id = var.cloudflare_zone_id
-  name    = each.value[0].name
-  type    = each.value[0].type
-  content = each.value[0].value
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.value
   ttl     = 60
   proxied = false
 }
@@ -36,7 +36,7 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn = aws_acm_certificate.cert.arn
 
   validation_record_fqdns = [
-    for r in cloudflare_dns_record.acm_validation : r.name
+    for record in cloudflare_dns_record.acm_validation : record.name
   ]
 
   depends_on = [cloudflare_dns_record.acm_validation]
