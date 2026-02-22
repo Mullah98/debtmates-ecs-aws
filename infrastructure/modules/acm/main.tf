6ace6e7e70404bdd0b721e151e9bed1c288fa072
Mullah98 <ibz_374@hotmail.com>
2026-02-22 14:53:30 +0000

    initial commit

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -14,12 +14,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "cloudflare_dns_record" "acm_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options :
-    dvo.resource_record_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
+    "cert_validation" = aws_acm_certificate.cert.domain_validation_options[0]
   }
 
   zone_id = var.cloudflare_zone_id
