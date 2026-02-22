a269793118532246d2fcc2929c5c089959dd7c99
Mullah98 <ibz_374@hotmail.com>
2026-02-22 14:51:07 +0000

    change loop name from domain name to resource domain

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -15,7 +15,7 @@ resource "aws_acm_certificate" "cert" {
 resource "cloudflare_dns_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options :
-    dvo.domain_name => {
+    dvo.resource_record_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
