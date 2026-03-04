a6e588fcddec34a51f5e7442883bbc5e7e481741
Mullah98 <ibz_374@hotmail.com>
2026-03-04 15:38:54 +0000

    remove latest tags from being pushed to ecr. add policies to ecr module

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "ecr" {
   name                 = var.ecr_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -9,4 +9,35 @@ resource "aws_ecr_repository" "ecr" {
   tags = {
     Name = "ecr-${var.ecr_name}"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Remove untagged images after 1 day"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep last 3 tagged images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["sha-"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 3
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
 }
