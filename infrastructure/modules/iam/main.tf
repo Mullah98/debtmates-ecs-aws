541293c166aaec5b1e369ab70720020c782e5ac7
Mullah98 <ibz_374@hotmail.com>
2026-03-10 03:30:00 +0000

    rename policy and role for clarity

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -4,8 +4,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "github-actions-role"
+resource "aws_iam_role" "terraform_role" {
+  name = "terraform-role"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [{
@@ -19,16 +19,16 @@ resource "aws_iam_role" "github_actions_role" {
           "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
         },
         "StringLike" : {
-          "token.actions.githubusercontent.com:sub" : "repo:${var.github_org}/${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" : "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"
         }
       }
     }]
   })
 }
 
-resource "aws_iam_role_policy" "github_actions_policy" {
-  name = "github-actions-policy"
-  role = aws_iam_role.github_actions_role.id
+resource "aws_iam_role_policy" "terraform_policy" {
+  name = "terraform-policy"
+  role = aws_iam_role.terraform_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
